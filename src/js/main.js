42541f8687a245364bb586bf1ec03a403835b29c
prosp3ro <prospero1135@gmail.com>
2019-06-06 12:11:18 +0300

    header + fixes

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,18 @@ import objectFitImages from "object-fit-images";
 $(function() {
   objectFitImages();
 });
+
+$(document).ready(function(){
+  $('.slider').slick({
+    infinite: true,
+    dots: true,
+    dotsClass: 'custom-dots',
+    arrows: true,
+    prevArrow: '<div class="arrow arrow--left"></div>',
+    nextArrow: '<div class="arrow arrow--right"></div>',
+    autoplay: true,
+    autoplaySpeed: 4000,
+    slidesToShow: 1,
+    slidesToScroll: 1,  
+  });
+});
