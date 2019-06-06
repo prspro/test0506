ca929c1a2ccf11f1e1ae3b9815bfe92cc6687211
prosp3ro <prospero1135@gmail.com>
2019-06-06 19:47:48 +0300

    responsive

--- src/js/main.js
+++ src/js/main.js
@@ -17,6 +17,14 @@ $(document).ready(function(){
     autoplay: true,
     autoplaySpeed: 4000,
     slidesToShow: 1,
-    slidesToScroll: 1,  
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          arrows: false
+        }
+      }
+    ]  
   });
 });
